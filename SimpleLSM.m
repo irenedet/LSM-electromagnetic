55c39f8c15556921c2458a54306a076eb1d08e1c
Irene de Teresa <irene@wifi-roaming-128-4-110-182.host.udel.edu>
2017-05-05 13:25:45 -0400

    change simpleLSM.m to plot the cube

--- SimpleLSM.m
+++ SimpleLSM.m
@@ -1,9 +1,10 @@
 %
-%  Simple LSM with fixed regularization parameter
+% Simple LSM with Ridge regression
 %
-clear all
-close all
-% Read data from FFP.txt
+%clear all
+%close all
+% Read data from FFP.txt to build prediction matrix
+% ||Ax-b||^2 + alpha ||x||^2
 [Points,npoints,tris,ntris,indexes,dv,pv,nb,ka,FFmat]=readff(1);
 
 A=zeros(2*nb,2*nb);
@@ -33,8 +34,18 @@ for m=1:nb
         A(nb+l,nb+m)=pv(:,2*l)'*FF2(:,l);%M_phiphi
     end
 end
-%%
-[SSpoints,Nsample,RHSmat]=readrhs(1,nb);
+disp(['The prediction matrix A has been assembled.'])
+%% 1.
+disp(['cond(A)=',num2str(cond(A))])
+%% 2. Noise matrix (to avoid numerical crimes and account for measurement errors)
+eps_noise=0.0001;
+Noise_mat=eps_noise*(-eye(size(A))+2*rand(size(A)));% uniform random noise
+Anoisy=A+Noise_mat;
+noise=norm(Noise_mat)/norm(A);
+disp(['The level of noise in this experiment is: ',num2str(100*noise),'%'])
+A=Anoisy;
+%% 3. Assemble right-hand-side 
+[SSpoints,Nsample,RHSmat]=readrhs(1,nb,folder_rhs);
 disp(['Using ',num2str(Nsample),' sampling points in each direction'])
 xx=SSpoints(1,:);
 yy=SSpoints(2,:);
@@ -49,34 +60,10 @@ for j=1:Nsample
     for iq=1:2
         q=[0;0;0];
         q(iq)=1;
-%         if (z(1)==0.6)||(z(1)==-0.6) 
-%             if (iq ==1)
-%                 vec=[1,0,0];
-%             elseif (iq==2)
-%                 vec=[0,1,0];
-%             end
-%         elseif (z(2)==0.6)||(z(2)==-0.6)
-%             if (iq ==1)
-%                 vec=[1,0,0];
-%             elseif (iq==2)
-%                 vec=[0,0,1];
-%             end
-%         elseif (z(3)==0.6)||(z(3)==-0.6)
-%             if (iq==1)
-%                 vec=[0,0,1];
-%             elseif (iq==2)
-%                 vec=[0,1,0];
-%             end
-%         end
-        
-    
+
         for l=1:nb
-%             b(l)=q'*RHSmat{l,1}(:,j);% q'*E_b(z_j,-dv_l,pv1)
-%             b(l+nb)=q'*RHSmat{l,2}(:,j);
-            vec=cross(dv(:,2*l-1),cross(q,dv(:,2*l-1)));%this has to be changed for non-homogeneous background!
-            b(l)=1j*ka*(pv(:,2*l-1)'*vec)*exp(-1i*ka*(dv(:,2*l-1)'*z));
-            vec=cross(dv(:,2*l),cross(q,dv(:,2*l)));
-            b(l+nb)=1j*ka*(pv(:,2*l)'*vec)*exp(-1i*ka*(dv(:,2*l)'*z));
+            b(l)=q'*RHSmat{l,1}(:,j);% q'*E_b(z_j,-dv_l,pv1)
+            b(l+nb)=q'*RHSmat{l,2}(:,j);
         end
         ball(:,iq)=b;
     end
@@ -91,19 +78,19 @@ for j=1:Nsample
         indicator(:,j)=[1,0,0]';
     end
 end
-%C=repmat()
 S=10*ones(1,Nsample);
 scatter3(xx,yy,zz,S,indicator');
-%% Plotting
-% h=patch(x(:,i),y(:,i),z(:,i),'k')
-% s=0.6;
-% x=[0 1 1 0 0 0;1 1 0 0 1 1;1 1 0 0 1 1;0 1 1 0 0 0]*s;
-% y=[0 0 1 1 0 0;0 1 1 0 0 0;0 1 1 0 1 1;0 0 1 1 1 1]*s;
-% z=[0 0 0 0 0 1;0 0 0 0 0 1;1 1 1 1 0 1;1 1 1 1 0 1]*s;
-% for i=1:6
-%     h=patch(x(:,i),y(:,i),z(:,i),'k');
-%     set(h,'edgecolor','w')
-% end
+%% Plotting the delamination (defective points)
+%h=patch(x(:,i),y(:,i),z(:,i),'k')
+figure
+s=1.2;
+x=[0 1 1 0 0 0;1 1 0 0 1 1;1 1 0 0 1 1;0 1 1 0 0 0]*s;
+y=[0 0 1 1 0 0;0 1 1 0 0 0;0 1 1 0 1 1;0 0 1 1 1 1]*s;
+z=[0 0 0 0 0 1;0 0 0 0 0 1;1 1 1 1 0 1;1 1 1 1 0 1]*s;
+for i=1:6
+    h=patch(x(:,i)-0.6,y(:,i)-0.6,z(:,i)-0.6,'k');
+    set(h,'edgecolor','b','FaceColor',[.3,.5,.3])
+end
 l=0.6*ones(1,Nsample);
 nosidex1=SSpoints(1,:)-l;nosidex2=SSpoints(1,:)+l;
 nosidey1=SSpoints(2,:)-l;nosidey2=SSpoints(2,:)+l;
@@ -114,11 +101,27 @@ sidey1=find((nosidey1==0));Sidey1=SSpoints(:,sidey1);
 sidey2=find((nosidey2==0));Sidey2=SSpoints(:,sidey2);
 sidez1=find((nosidez1==0));Sidez1=SSpoints(:,sidez1);
 sidez2=find((nosidez2==0));Sidez2=SSpoints(:,sidez2);
-for i=1:length(sidex1)
-     h=patch(Sidex1(1,:),Sidex1(2,:),Sidex1(3,:),'k');
-     set(h,'edgecolor','w')
- end
-%%
+%figure
+hold on
+% scatter3(Sidex1(1,:),Sidex1(2,:),Sidex1(3,:),'black');
+% scatter3(Sidex2(1,:),Sidex2(2,:),Sidex2(3,:),'black');
+% scatter3(Sidey1(1,:),Sidey1(2,:),Sidey1(3,:),'black');
+% scatter3(Sidey2(1,:),Sidey2(2,:),Sidey2(3,:),'black');
+% scatter3(Sidez1(1,:),Sidez1(2,:),Sidez1(3,:),'black');
+% scatter3(Sidez2(1,:),Sidez2(2,:),Sidez2(3,:),'black');
+ginvx1=zeros(size(Sidex1));ginvx1(1,:)=50*ginv(sidex1);Ginvx1=Sidex1+ginvx1;
+ginvx2=zeros(size(Sidex2));ginvx2(1,:)=50*ginv(sidex2);Ginvx2=Sidex2-ginvx2;
+ginvy1=zeros(size(Sidey1));ginvy1(2,:)=50*ginv(sidey1);Ginvy1=Sidey1+ginvy1;
+ginvy2=zeros(size(Sidey2));ginvy2(2,:)=50*ginv(sidey2);Ginvy2=Sidey2-ginvy2;
+ginvz1=zeros(size(Sidez1));ginvz1(3,:)=50*ginv(sidez1);Ginvz1=Sidez1+ginvz1;
+ginvz2=zeros(size(Sidez2));ginvz2(3,:)=50*ginv(sidez2);Ginvz2=Sidez2-ginvz2;
+scatter3(Ginvx1(1,:),Ginvx1(2,:),Ginvx1(3,:),'b');
+scatter3(Ginvx2(1,:),Ginvx2(2,:),Ginvx2(3,:),'b');
+scatter3(Ginvy1(1,:),Ginvy1(2,:),Ginvy1(3,:),'b');
+scatter3(Ginvy2(1,:),Ginvy2(2,:),Ginvy2(3,:),'b');
+scatter3(Ginvz1(1,:),Ginvz1(2,:),Ginvz1(3,:),'red');
+scatter3(Ginvz2(1,:),Ginvz2(2,:),Ginvz2(3,:),'b');
+%% !!!!
 % Lots of test points
 nxx=41;
 %lsnxx=21
@@ -157,12 +160,20 @@ ginv=reshape(ginv,nxx,nxx,nxx);
 zX=reshape(zX,nxx,nxx,nxx);
 zY=reshape(zY,nxx,nxx,nxx);
 zZ=reshape(zZ,nxx,nxx,nxx);
-
-level = 0.01;
+%% 4.
+level = 0.4;
 figure
 slice(zX,zY,zZ,ginv,0,0,0)
+%figure
+hold on
+s=1.2;
+x=[0 1 1 0 0 0;1 1 0 0 1 1;1 1 0 0 1 1;0 1 1 0 0 0]*s;
+y=[0 0 1 1 0 0;0 1 1 0 0 0;0 1 1 0 1 1;0 0 1 1 1 1]*s;
+z=[0 0 0 0 0 1;0 0 0 0 0 1;1 1 1 1 0 1;1 1 1 1 0 1]*s;
+for i=1:6
+    h=patch(x(:,i)-0.6,y(:,i)-0.6,z(:,i)-0.6,'k');
+    set(h,'edgecolor','b','FaceColor',[1,1,1])
+end
 hold on
 isosurface(zX,zY,zZ,ginv,level*(max(max(max(ginv)))-min(min(min(ginv))))+min(min(min(ginv))))
 hold off
-
-
